b3a3d0bf787c5846288b843a6e23ffebc28d0f47
David Winslow <dwinslow@radiantblue.com>
2015-10-23 08:50:39 -0400

    Fix copy_builds.sh to reflect move of services subproject

--- copy_builds.sh
+++ copy_builds.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-cp ../geoint_messaging/uploader/target/universal/uploader-0.1-SNAPSHOT.tgz ansible/roles/uploader/files/
-cp ../geoint_messaging/ogcproxy/target/universal/ogcproxy-0.1-SNAPSHOT.tgz ansible/roles/ogcproxy/files/
-cp ../geoint_messaging/deployer/target/universal/deployer-0.1-SNAPSHOT.tgz ansible/roles/deployer/files/
-cp ../geoint_messaging/normalizer/target/scala-2.10/normalizer-assembly-0.1-SNAPSHOT.jar ansible/roles/normalizer/files/
+cp services/uploader/target/universal/uploader-0.1-SNAPSHOT.tgz ansible/roles/uploader/files/
+cp services/ogcproxy/target/universal/ogcproxy-0.1-SNAPSHOT.tgz ansible/roles/ogcproxy/files/
+cp services/deployer/target/universal/deployer-0.1-SNAPSHOT.tgz ansible/roles/deployer/files/
+cp services/normalizer/target/scala-2.10/normalizer-assembly-0.1-SNAPSHOT.jar ansible/roles/normalizer/files/
